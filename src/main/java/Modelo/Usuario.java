8f2209986b317a33985d722537027e02b700b6d0
Matías <124216748+Other5ide@users.noreply.github.com>
2025-06-12 09:21:11 -0400

    incorporacion de unused object

--- src/main/java/Modelo/Usuario.java
+++ src/main/java/Modelo/Usuario.java
@@ -3,7 +3,6 @@ package Modelo;
 public class Usuario {
     private String nombre;
     private String clave;
-    private Perfil perfil;
 
     public Usuario(String nombre, String clave) {
         this.nombre = nombre;
